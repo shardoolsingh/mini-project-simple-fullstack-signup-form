80b3cc396132fe8db2c76a5739a6fad3cdb753c1
Shardool Singh <shardoolsingh4@gmail.com>
2025-06-06 01:07:09 +0530

    Major: implement the basic backend logic to signup form app for crud operations using express prisma sqlite

---
+++ backend/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+// implement routes
+
+// show a welcome message at home route
+app.get('/', (req, res) => {
+	res.send("Welcome to our dashboard!");
+});
+
+// create users
+app.post('/users', async (req, res) => {
+	const { name, email, password } = req.body;
+	const user = await prisma.users.create({
+		data: { name, email, password },
+	});
+
+	res.json(user);
+});
+
+// read all users
+app.get('/users', async (req, res) => {
+	const users = await prisma.users.findMany();
+
+	res.json(users);
+});
+
+// read a single user
+app.get('/users/:id', async (req, res) => {
+	const user = await prisma.users.findUnique({
+		where: { id: parseInt(req.params.id) },
+	});
+
+	res.json(user);
+});
+
+// update a single user data
+app.put('/users/:id', async (req, res) => {
+	const { name, email, password } = req.body;
+	const user = await prisma.users.update({
+		where: {  id: parseInt(req.params.id) },
+		data: { name, email, password },
+	});
+
+	res.json(user);
+});
+
+// delete a user
+app.delete('/users/:id', async (req, res) => {
+	const user = await prisma.users.delete({
+		where: { id: parseInt(req.params.id) },
+	});
+
+	res.json(user);
+});
+
+// start the server
+const PORT = 3003;
+app.listen(PORT, () => {
+	console.log(`Express server running at http://localhost:${PORT}`);
+});
---
+++ backend/prisma/migrations/20250605180407_init/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `password` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+INSERT INTO "new_users" ("email", "id", "name") SELECT "email", "id", "name" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
