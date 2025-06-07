5af7ec754ebcbefaa1783ba3bac055127723415d
Shardool Singh <shardoolsingh4@gmail.com>
2025-06-07 20:13:59 +0530

    Major: add frontend by creating a simple html signup form & connect it with backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const app = express();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-app.use(express.json());
+//app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 // implement routes
 
